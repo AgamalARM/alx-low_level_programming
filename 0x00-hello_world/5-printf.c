0ef4f7c860c96005a725f852f42dd66abdbb3762
AgamalARM <agamalarm@gmail.com>
2023-06-15 12:31:17 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: the C program using printf function to print string
+ *
+ * Return: 0 for success
+*/
+
+int main(voi)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
