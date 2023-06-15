d2bba492994433048cbbe1ee171628a7d0820a9d
AgamalARM <agamalarm@gmail.com>
2023-06-15 12:05:42 -0700

    puts function to print string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main Entry point
+ *
+ * Description: the C programe using puts function to print a string
+ *
+ * Return: 0 for success
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
