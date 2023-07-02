e3e5583f18953def6c8199f2277b0b427ce5d59a
AgamalARM <agamalarm@gmail.com>
2023-07-02 12:51:35 -0700

    more pointer

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,26 @@
 #include "main.h"
-/**
- *_strcat - concatenates two string
- *@dest: A pointer to a character to be changed
- *@src: A pointer to a character to be changed
- *Return: dest
- */
 
+/**
+ * _strcat - concat 2 string
+ * @dest:char
+ * @src:char
+ * Return:char
+ */
 char *_strcat(char *dest, char *src)
 {
-int i, j;
+	char *s = dest;
 
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
+	while (*dest != '\0')
+	{
+		dest++;
+	}
 
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
-
-return (dest);
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
 }
